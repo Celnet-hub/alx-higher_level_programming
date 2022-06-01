75029ed901285fd6aece80fe68a31a097308f834
celestine <nwabuisicelestine@gmail.com>
2022-06-01 02:20:37 +0100

    updated readme file and corrected code format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,7 +12,7 @@ if number > 0:
         print(f'Last digit of {number} is {lst_digit} and is {lst_digit}')
     elif lst_digit < 6 and lst_digit != 0:
         print(f'Last digit of {number} is {lst_digit} and \
-            is less than 6 and not 0')
+is less than 6 and not 0')
 # Negative numbers
 else:
     lst_digit = number % -10
@@ -22,4 +22,4 @@ else:
         print(f'Last digit of {number} is {lst_digit} and is {lst_digit}')
     elif lst_digit < 6 and lst_digit != 0:
         print(f'Last digit of {number} is {lst_digit} and \
-            is less than 6 and not 0')
+is less than 6 and not 0')
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -3,3 +3,7 @@
 ## 0-positive_or_negative.py
 
 - Checks if a number is either positive or negative or zero then prints the result.
+
+## 1-last_digit.py
+
+- gets the last digit of a number.
