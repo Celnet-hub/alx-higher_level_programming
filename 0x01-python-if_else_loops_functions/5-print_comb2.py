7466dbd7994a5b360867ef22d4f876bac085dae4
celestine <nwabuisicelestine@gmail.com>
2022-06-01 17:59:15 +0100

    added and extra print condition

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 # {:02d} is used to print out a number with 2 digits
-for i in range(0, 100):
+for i in range(0, 99):
     print("{:02d}".format(i), end=", ")
+print("{}".format(99))
